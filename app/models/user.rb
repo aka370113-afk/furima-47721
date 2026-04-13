1792c426b53efcc29eb34d0907ba3ba37d5fc1e3
aka370113-afk <aka370113@gmail.com>
2026-04-13 22:29:10 +0900

    user.rbバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         validates :family_name_kanji, presence: true
+         validates :given_name_kanji, presence: true
+         validates :family_name_kana, presence: true
+         validates :given_name_kana, presence: true
+         validates :nickname, presence: true
+         validates :birth_date, presence: true
+
+          has_many :items
+          has_many :purchases
+
 end
