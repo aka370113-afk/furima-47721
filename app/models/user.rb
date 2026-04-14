bc0c175f472b4950eff398f4dc169aff1cb9beef
aka370113-afk <aka370113@gmail.com>
2026-04-14 16:05:22 +0900

    エラーハンドリング・ユーザー・本人情報

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,29 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         validates :family_name_kanji, presence: true
-         validates :given_name_kanji, presence: true
-         validates :family_name_kana, presence: true
-         validates :given_name_kana, presence: true
-         validates :nickname, presence: true
-         validates :birth_date, presence: true
+  PASSWORD_REGEX = /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/.freeze
 
-          has_many :items
-          has_many :purchases
+  validates :password,
+            format: { with: PASSWORD_REGEX, message: "は半角英数字混合で入力してください" },
+            allow_blank: true
 
+  with_options presence: true do
+    # ひらがな、カタカナ、漢字のみ許可する（名字・名前）
+    validates :family_name_kanji,
+              format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "は全角（漢字・ひらがな・カタカナ）で入力してください", allow_blank: true }
+    validates :given_name_kanji,
+              format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "は全角（漢字・ひらがな・カタカナ）で入力してください", allow_blank: true }
+    # カタカナのみ許可する（名字カナ・名前カナ）
+    validates :family_name_kana,
+              format: { with: /\A[ァ-ヶー]+\z/, message: "は全角カタカナで入力してください", allow_blank: true }
+    validates :given_name_kana,
+              format: { with: /\A[ァ-ヶー]+\z/, message: "は全角カタカナで入力してください", allow_blank: true }
+    # 半角英字数字のみ許可する
+    validates :nickname,
+              format: { with: /\A[a-z0-9]+\z/i, message: "は半角英字数字で入力してください", allow_blank: true }
+    validates :birth_date
+  end
+
+  has_many :items
+  has_many :purchases
 end
