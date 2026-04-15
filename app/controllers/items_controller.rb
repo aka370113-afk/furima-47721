c099d09207cbc24ff241a08e3dabce92bc7d9a18
aka370113-afk <aka370113@gmail.com>
2026-04-15 12:48:35 +0900

    商品情報がデータベースに保存される

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,24 @@
 class ItemsController < ApplicationController
+
+  before_action :authenticate_user!, only: [:new, :create] # ログインしていないユーザーを出品画面に行かせない
+
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path # 保存できたらトップページへ
+    else
+      render :new, status: :unprocessable_entity # 保存失敗したら入力画面に戻る
+    end
+  end
+
+private
 
 def item_params
   params.require(:item).permit(
