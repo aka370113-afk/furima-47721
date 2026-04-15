33d1de77ad47a624549d02ac9f0e3f5fff71602a
aka370113-afk <aka370113@gmail.com>
2026-04-15 11:36:32 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,18 @@
 class ItemsController < ApplicationController
   def index
   end
+
+def item_params
+  params.require(:item).permit(
+    :item_name,
+    :item_info,
+    :item_price,
+    :category_id,
+    :item_status_id,
+    :shipping_fee_status_id,
+    :prefecture_id,
+    :scheduled_delivery_id,
+    :image
+  ).merge(user_id: current_user.id)
+end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -25,4 +25,6 @@ class User < ApplicationRecord
     validates :birth_date
   end
 
+  has_many :items
+
 end
