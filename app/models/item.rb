8d1866016ec29251d401b5b4f5dfa7e84d28d6f2
aka370113-afk <aka370113@gmail.com>
2026-04-23 14:42:47 +0900

    多機能関連

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,6 +13,10 @@ class Item < ApplicationRecord
   has_one_attached :image
   has_one :purchase, dependent: :destroy
 
+  def sold?
+    purchase.present?
+  end
+
   validates :image, presence: true
   validates :item_name, presence: true
   validates :item_info, presence: true
